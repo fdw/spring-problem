30c7f767d848532036486b6ff79fcea12faab4ab
Rob Winch <rwinch@users.noreply.github.com>
2021-02-25 15:20:01 -0600

    Expose the UserDetailsManager as a Bean
    Previously the configure method is used which means that the
    UserDetailsService is isolated to the specific
    WebSecurityConfigurerAdapter.
    
    This step removes AuthenticationManagerBuilder which is not the
    recommended approach in favor of exposing the UserDetailsService as a
    Bean.

--- parent/src/main/kotlin/com/example/config/SecurityConfiguration.kt
+++ parent/src/main/kotlin/com/example/config/SecurityConfiguration.kt
@@ -3,6 +3,7 @@ package com.example.config
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
 import org.springframework.boot.actuate.info.InfoEndpoint
 import org.springframework.boot.actuate.metrics.export.prometheus.PrometheusScrapeEndpoint
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
@@ -10,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
 
 private object Roles {
     const val ADMIN = "ADMIN"
@@ -32,25 +35,23 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
             .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole(Roles.ADMIN)
     }
 
-    override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.inMemoryAuthentication()
-            .withUser(
-                User.builder().username("admin")
-                    .password("verysecretpassword")
-                    .roles(Roles.ADMIN)
-                    .build()
-            )
-            .withUser(
-                User.builder().username("infra")
-                    .password("widelyknownpassword")
-                    .roles(Roles.INFRA)
-                    .build()
-            )
-            .withUser(
-                User.builder().username("user")
-                    .password("whatsapassword")
-                    .roles(Roles.USER)
-                    .build()
-            )
+    @Bean
+    fun parentUserDetails() : UserDetailsService {
+        val admin = User.builder()
+                .username("admin")
+                .password("verysecretpassword")
+                .roles(Roles.ADMIN)
+                .build()
+        val infra = User.builder()
+                .username("infra")
+                .password("widelyknownpassword")
+                .roles(Roles.INFRA)
+                .build()
+        val user = User.builder()
+                .username("user")
+                .password("whatsapassword")
+                .roles(Roles.USER)
+                .build()
+        return InMemoryUserDetailsManager(admin, infra, user);
     }
 }
